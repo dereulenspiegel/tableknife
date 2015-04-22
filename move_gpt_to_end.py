168715c6ee58788d15bac9104951c8ccc71573f9
Till Klocke <till.klocke@yetu.com>
2015-04-22 16:22:52 +0200

    Secondary header is now at the end of the device

--- move_gpt_to_end.py
+++ move_gpt_to_end.py
@@ -4,7 +4,7 @@ from gpt_lib import BlockDev
 from gpt_lib import GPT
 
 def main():
-	device = BlockDev('')
+	device = BlockDev('gpt_full.img')
 	gpt = GPT(device)
 
 	primary_header = gpt.get_gpt_header()
@@ -13,9 +13,10 @@ def main():
 	gpt_entries = gpt.get_table()
 
 	block_size = device.get_block_count()
+	print 'Determined block_size of %d blocks'%block_size
 
-	secondary_header_offset = block_size - 33
-	secondary_table_offset = block_size - 32
+	secondary_header_offset = block_size - 1
+	secondary_table_offset = block_size - 33
 	last_usable_lba = block_size - 34
 
 	primary_header.last_usable_lba = last_usable_lba
